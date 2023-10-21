91586144773b8682496d48198f84a92f10cd6334
Fabian Botero <boterofabian@americana.edu.co>
2023-10-21 11:49:49 -0500

    Adicion de Docker y Web Service

--- web-server/main.py
+++ web-server/main.py
@@ -1,5 +1,33 @@
 import store
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return[1,2,3]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_contact():
+    return """
+            <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Pagina Web</title>
+                </head>
+                <body>
+                    <h1>Esta es una Pagina</h1>
+                    <h2>Retornada por un Web Service</h2>
+                    <br>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis eveniet, ab temporibus minima tempore autem dolorum quo quidem iure nam quam aliquid molestiae in sed excepturi explicabo, sapiente sunt culpa.</p>
+                </body>
+                </html>
+"""
+
+
 
 def run():
     store.get_categories()
